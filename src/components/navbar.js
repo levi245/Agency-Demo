2301090801017479b11c2aaf5eaeb2756393eb17
levi245 <baltazarlevi12@gmail.com>
2021-08-03 15:16:40 +0800

    add link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,7 +14,13 @@ function navbar() {
         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
+        <a className="nav-link" href="#">About Us</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
       </li>
       
     </ul>
