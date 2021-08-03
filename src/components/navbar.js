6f6d1b584175221a62b35ecf457647a605846865
levi245 <baltazarlevi12@gmail.com>
2021-08-03 16:19:46 +0800

    background change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from '../human-resources.png'
 function navbar() {
     return (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
     <div class="container">
   
 
