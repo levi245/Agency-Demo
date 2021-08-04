6214ae33a0b6e4eadd3c350da096f442da1f0e6a
levi245 <baltazarlevi12@gmail.com>
2021-08-04 20:34:27 +0800

    another changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/navbar';
+import Header from './components/header';
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <Header />
        
       
     </div>
